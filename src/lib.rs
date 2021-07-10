8ed1125ed5b86e75a1f9d27090d4e2af3bb5d4fc
ketenburhan <ketenburhan@protonmail.com>
2021-07-10 15:15:51 +0300

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 mod utils;
 
+use js_sys::Math::random;
+
 use wasm_bindgen::prelude::*;
+use std::fmt;
+
+use fixedbitset::FixedBitSet;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +14,85 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+	(row * self.width + col) as usize
+    }
+    fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
+	let mut count = 0;
+	for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+	    for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+		if delta_row == 0 && delta_col == 0 {
+		    continue;
+		}
+
+		let neighbor_row = (row + delta_row) % self.height;
+		let neighbor_col = (col + delta_col) % self.width;
+
+		let index = self.get_index(neighbor_row, neighbor_col);
+
+		count += self.cells[index] as u8;
+	    }
+	}
+	count
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(format!("Hello, {}!", name).as_str());
+impl Universe {
+    pub fn tick(&mut self) {
+	let mut next_buffer = self.cells.clone();
+
+	for row in 0..self.height {
+	    for col in 0..self.width {
+		let index = self.get_index(row, col);
+		let cell = self.cells[index];
+
+		let live_neighbors = self.live_neighbor_count(row, col);
+
+		next_buffer.set(index, match (cell, live_neighbors) {
+		    (true, x) if x < 2 => false,
+		    (true, 2|3) => true,
+		    (true, x) if x > 3 => false,
+		    (false, 3) => true,
+		    (keep, _) => keep,
+		});
+	    }
+	}
+	self.cells = next_buffer;
+    }
+    pub fn new() -> Universe {
+	let width = 64;
+	let height = 64;
+
+	let size = (width * height) as usize;
+	let mut cells = FixedBitSet::with_capacity(size);
+
+	for i in 0..size {
+	    cells.set(i, random() < 0.5);
+	}
+
+	Universe {
+	    width,
+	    height,
+	    cells,
+	}
+    }    
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,82 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-wasm.greet("burhan");
+const CELL_SIZE = 8;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+const uni = Universe.new();
+const width = uni.width();
+const height = uni.height();
+
+const canvas = document.querySelector("#game-of-life-canvas");
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+
+const c = canvas.getContext("2d");
+
+
+const loop = () => {
+    uni.tick();
+
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(loop);
+};
+
+function drawGrid() {
+    c.beginPath();
+    c.strokeStyle = GRID_COLOR;
+
+    for (let i = 0; i <= width; i++) {
+	const x = i * (CELL_SIZE + 1) + 1;
+	c.moveTo(x, 0);
+	c.lineTo(x, canvas.height);
+    }
+    
+    for (let i = 0; i <= height; i++) {
+	const y = i * (CELL_SIZE + 1) + 1;
+	c.moveTo(0, y);
+	c.lineTo(canvas.width, y);
+    }
+    c.stroke();
+}
+
+const getIndex = (row, col) => row * width + col;
+
+const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << (n % 8);
+    return (arr[byte] & mask) === mask;
+}
+
+function drawCells() {
+    const cellsPtr = uni.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width*height / 8);
+
+    c.beginPath();
+
+    for (let row = 0; row < height; row++) {
+	for (let col = 0; col < width; col++) {
+	    const index = getIndex(row, col);
+
+	    c.fillStyle = bitIsSet(index, cells)
+		? ALIVE_COLOR
+		: DEAD_COLOR;
+
+	    c.fillRect(
+		(col * (CELL_SIZE + 1) + 1),
+		(row * (CELL_SIZE + 1) + 1),
+		CELL_SIZE,
+		CELL_SIZE,
+	    );
+	}	
+    }
+
+    c.stroke();
+}
+
+// start loop
+requestAnimationFrame(loop);
